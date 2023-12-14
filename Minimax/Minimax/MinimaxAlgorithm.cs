02e9ad15949d7ffd81ce9c37cce355db4dd7c0e4
Naumych12 <65111481+Naumych12@users.noreply.github.com>
2023-12-14 23:05:49 +0100

    Add files via upload

---
+++ Minimax/Minimax/MinimaxAlgorithm.cs
@@ -0,0 +1,137 @@
+﻿using System.Diagnostics;
+
+namespace Minimax;
+
+static class MinimaxAlgorithm
+{
+    public static (double, long) FindMinimax(double[,] matrix)
+    {
+        var rows = matrix.GetLength(0);
+        var cols = matrix.GetLength(1);
+        
+        Stopwatch stopwatch = new();
+        stopwatch.Start();
+
+        double[] rowMax = new double[rows];
+        for (int i = 0; i < rows; i++)
+        {
+            double maxInRow = double.MinValue;
+            for (int j = 0; j < cols; j++)
+            {
+                if (matrix[i, j] > maxInRow)
+                {
+                    maxInRow = matrix[i, j];
+                }
+            }
+            rowMax[i] = maxInRow;
+        }
+
+        double minimax = rowMax[0];
+        for (int i = 1; i < rows; i++)
+        {
+            if (rowMax[i] < minimax)
+            {
+                minimax = rowMax[i];
+            }
+        }
+
+        stopwatch.Stop();
+        return (minimax, stopwatch.ElapsedTicks);
+    }
+    
+    public static (double, long, int) FindMinimaxParallel(double[,] matrix, int threadsAmount)
+    {
+        var rows = matrix.GetLength(0);
+        var cols = matrix.GetLength(1);
+
+        if (threadsAmount <= 0)
+        {
+            threadsAmount = Environment.ProcessorCount * 12 / 7;
+        }
+
+        if (threadsAmount > rows)
+        {
+            threadsAmount = rows;
+        }
+
+        var rowsPerOneThread = rows / threadsAmount;
+        
+        var allThreads = new List<int>();
+        for (int i = 0; i < threadsAmount; i++)
+        {
+            allThreads.Add(rowsPerOneThread);
+        }
+        allThreads[^1] += rows % threadsAmount;
+
+        var startedThreadsAmount = 0;
+        var resetEvent = new ManualResetEvent(false);
+        
+        Stopwatch stopwatch = new();
+        stopwatch.Start();
+        
+        double[] rowMax = new double[rows];
+        for (int threadI = 0; threadI < threadsAmount; threadI++)
+        {
+            var currentI = threadI;
+            var thread = new Thread(() =>
+            {
+                for (int i = currentI * rowsPerOneThread; i < currentI * rowsPerOneThread + allThreads[currentI]; i++)
+                {
+                    double maxInRow = double.MinValue;
+                    for (int j = 0; j < cols; j++)
+                    {
+                        if (matrix[i, j] > maxInRow)
+                        {
+                            maxInRow = matrix[i, j];
+                        }
+                    }
+                    rowMax[i] = maxInRow;
+                }
+
+                
+                if (Interlocked.Increment(ref startedThreadsAmount) == threadsAmount)
+                {
+                    resetEvent.Set();
+                }
+            });
+            
+            thread.Start();
+        }
+
+        resetEvent.WaitOne();
+        resetEvent.Reset();
+        startedThreadsAmount = 0;
+        var lockObject = new object();
+        
+        double minimax = rowMax[0];
+        for (int i = 0; i < threadsAmount; i++)
+        {
+            var currentI = i;
+            var thread = new Thread(() =>
+            {
+                for (int j = currentI * rowsPerOneThread; j < currentI * rowsPerOneThread + allThreads[currentI]; j++)
+                {
+                    if (rowMax[j] < minimax)
+                    {
+                        lock (lockObject)
+                        {
+                            minimax = rowMax[j];
+                        }
+                    }
+                }
+                
+                if (Interlocked.Increment(ref startedThreadsAmount) == threadsAmount)
+                {
+                    resetEvent.Set();
+                }
+            });
+            
+            thread.Start();
+        }
+        
+        resetEvent.WaitOne();
+        stopwatch.Stop();
+        
+        return (minimax, stopwatch.ElapsedTicks, threadsAmount);
+    }
+}
---
+++ Minimax/Minimax/Program.cs
@@ -0,0 +1,60 @@
+﻿using Minimax;
+
+
+var parametersList = new List<Parameters>
+{
+    new(10, 10, 5),
+    new(10, 10, 10),
+    
+    new(900, 900, 100),
+    new(900, 900, 200),
+    new(900, 900, 900),
+    
+    new(9000, 9000, 100),
+    new(9000, 9000, 300),
+    new(9000, 9000, 1000),
+    new(9000, 9000, 3000),
+    
+    new(900000, 1000, 500),
+};
+
+
+var random = new Random();
+foreach (var parameters in parametersList)
+{
+    var matrix = new double[parameters.RowsAmount,parameters.ColumnsAmount];
+    for (int i = 0; i < parameters.RowsAmount; i++)
+    {
+        for (int j = 0; j < parameters.ColumnsAmount; j++)
+        {
+            matrix[i, j] = random.Next(-999, 999);
+        }
+    }
+
+    var result1 = MinimaxAlgorithm.FindMinimax(matrix);
+    var result2 = MinimaxAlgorithm.FindMinimaxParallel(matrix, parameters.ThreadsAmount);
+    
+    Console.WriteLine();
+    Console.WriteLine("Для матрицi " + parameters.RowsAmount + "X" + parameters.ColumnsAmount);
+    Console.WriteLine("Мiнiмакс однопотокового: " + result1.Item1);
+    Console.WriteLine("Мiнiмакс багатопотокового: " + result2.Item1);
+    Console.WriteLine("Час виконання однопотокового алгоритму: " + result1.Item2 + " тiкiв");
+    Console.WriteLine("Час виконання багатопотокового алгоритму: " + result2.Item2 + " тiкiв");
+    Console.WriteLine("Потокiв задiяно: " + result2.Item3);
+    Console.WriteLine("Величина прискорення: " + result1.Item2 / (double)result2.Item2);
+}
+
+
+class Parameters
+{
+    public Parameters(int rowsAmount, int columnsAmount, int threadsAmount)
+    {
+        RowsAmount = rowsAmount;
+        ColumnsAmount = columnsAmount;
+        ThreadsAmount = threadsAmount;
+    }
+
+    public int RowsAmount { get; }
+    public int ColumnsAmount { get; }
+    public int ThreadsAmount { get; }
+}
